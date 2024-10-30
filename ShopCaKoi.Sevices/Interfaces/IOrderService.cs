2ab3af4f60a160e8c22816c9d84bee7d6f5e4115
Duong-member <duongnk0393@ut.edu.vn>
2024-10-30 22:51:18 +0700

    add IOrderSevrice , fix IOrderSevrice

---
+++ ShopCaKoi.Sevices/Interfaces/IOrderService.cs
@@ -0,0 +1,21 @@
+﻿using ShopCaKoi.Repositores.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCaKoi.Sevices.Interfaces
+{
+    internal interface IOrderService
+    {
+        Task<List<OrderKoi>> GetAllOrders();
+        Task<OrderKoi> GetOrderById(string orderId);
+        Task<bool> AddOrder(OrderKoi order);
+        Task<bool> UpdateOrder(OrderKoi order);
+        Task<bool> DeleteOrder(OrderKoi order);
+        Task<bool> DeleteOrder(string orderId);
+        Task<bool> UpdatePaymentStatus(string orderId, string paymentStatus);
+        bool OrderExists(string orderId);
+    }
+}
