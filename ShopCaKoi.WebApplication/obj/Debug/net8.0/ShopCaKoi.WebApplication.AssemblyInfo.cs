a2b6e7225a3b086d3bde9e029328915e7d1fe0ff
Viet-Leader <viettq2511@ut.edu.vn>
2024-10-31 23:42:35 +0700

    fix reponsitory

--- ShopCaKoi.WebApplication/obj/Debug/net8.0/ShopCaKoi.WebApplication.AssemblyInfo.cs
+++ ShopCaKoi.WebApplication/obj/Debug/net8.0/ShopCaKoi.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopCaKoi.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9646eb53545804059ee98e3fae8aebd770b53f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9c5968291af8198f57873bf8f4a09848db3f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopCaKoi.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopCaKoi.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
